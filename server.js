ee484dcc940ed90bbffbed51d5fdf2e5a3b3a7af
Hanny Tran <cheershanny@gmail.com>
2023-10-17 19:41:28 +0100

    document API endpoints

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const server = express();
 const bodyParser = require("body-parser");
+
 const userRoutes = require("./routes/user");
 const productRoutes = require("./routes/product");
 const loginRoutes = require("./routes/login");
 const registerRoutes = require("./routes/register");
 const profileRoutes = require("./routes/profile");
+
+const swaggerUI = require("swagger-ui-express");
+const yaml = require("yamljs");
+const swaggerDocument = yaml.load('api.yaml');
+server.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 const port = 3000;
 
@@ -21,9 +27,12 @@ server.get("/", (req, res) => {
 });
 
 server.use("/", loginRoutes, userRoutes, registerRoutes, productRoutes);
-server.use('/profile', profileRoutes);
+server.use("/profile", profileRoutes);
 
 // Start the server
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
+  console.log(
+    `Swagger-ui is available on http://localhost:${port}/api-docs`
+  );
 });
