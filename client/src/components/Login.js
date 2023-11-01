2d17068f04538d2c5b042f808865bb3911910007
Hanny Tran <cheershanny@gmail.com>
2023-11-01 15:08:59 +0000

    add Google sign in button

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { GoogleLogin } from "@react-oauth/google";
 
 function Login(props) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMsg, setErrorMsg] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -14,9 +15,9 @@ function Login(props) {
       const response = await fetch("/login", {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({ username, password })
+        body: JSON.stringify({ username, password }),
       });
 
       if (response.ok) {
@@ -35,23 +36,45 @@ function Login(props) {
   return (
     <div className="login-container">
       <h2>Login</h2>
-      
+
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label>Username:</label>
-          <input type="text" value={username} onChange={e => setUsername(e.target.value)} required />
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
         </div>
         <div className="form-group">
           <label>Password:</label>
-          <input type="password" value={password} onChange={e => setPassword(e.target.value)} required />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
         </div>
         <button type="submit">Login</button>
       </form>
       <div className="third-party-login">
-        <button onClick={() => window.location.href='/auth/google'}>Login with Google</button>
-        <button onClick={() => window.location.href='/auth/facebook'}>Login with Facebook</button>
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              console.log(credentialResponse);
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+          />
+
+        <button onClick={() => (window.location.href = "/auth/facebook")}>
+          Login with Facebook
+        </button>
       </div>
-      <p>Don't have an account? <a href="/register">Register here</a></p>
+      <p>
+        Don't have an account? <a href="/register">Register here</a>
+      </p>
       {errorMsg && <p className="error-message">{errorMsg}</p>}
     </div>
   );
