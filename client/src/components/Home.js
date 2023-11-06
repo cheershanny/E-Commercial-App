e60a946ac916eeaf19a0c110ff01e8dcfb4f39ce
Hanny Tran <cheershanny@gmail.com>
2023-11-06 11:18:10 +0000

    show all products on home page and style it

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+  const [productData, setProductData] = useState([]);
+  useEffect(() => {
+    fetch('/products')
+    .then((res) => res.json())
+    .then((data) => setProductData(data))
+    .catch((err) => console.err("Error fetching product:", err))
+  })
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Something pretty for your home</h1>
+      <ul className='product-list'>
+        {productData.map((product) => (
+          <li key={product.product_id} className="product-item" >
+            <span className="product-name">{product.product_name}</span> 
+            <span className="product-description">{product.description}</span>
+            <span className="product-price">{product.price}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
